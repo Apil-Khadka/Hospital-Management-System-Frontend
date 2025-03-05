5b73d50eb37081153e1ae80f8566cf034423aa34
Apil Khadka <apilkhadka2020@gmail.com>
2025-03-05 12:09:22 +0545

    server

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
